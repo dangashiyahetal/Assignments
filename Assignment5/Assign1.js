deed0a66a468f23d24525fcfc2bf1023bae411bc
Hetal Dangashia <dangashiyahetal@gmail.com>
2022-03-15 20:10:10 -0400

    Assignment 5

---
+++ Assignment5/Assign1.js
@@ -0,0 +1,24 @@
+function Calculate() {
+  var x = 13;
+  var y = 5;
+  var sum = x + y;
+  var multipy = x * y;
+  var modulus = x % y;
+  alert("Sum of " + x + " and " + y + " = " + sum);
+  alert("multiply of " + x + " and " + y + " = " + multipy);
+  alert("modulus of " + x + " and " + y + " = " + modulus);
+}
+
+function sum() {
+  var x = 10;
+  var y = 5;
+  var sum = x + y;
+  alert("sum of " + x + " and " + y + " = " + sum);
+}
+
+function multiply() {
+  var x = 10;
+  var y = 5;
+  var multiply = x * y;
+  alert("multiply " + x + " and " + y + " = " + sum);
+}
