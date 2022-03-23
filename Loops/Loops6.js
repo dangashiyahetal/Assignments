7a875a08a3d3cff0fab75a89f5a07eeea1b67024
Hetal Dangashiya <dangashiyahetal@gmail.com>
2022-03-23 19:16:03 -0400

    Removed console log

--- Loops/Loops6.js
+++ Loops/Loops6.js
@@ -1,18 +1,11 @@
 (function () {
   for (var counter = 2; counter <= 100; counter++) {
     var notPrime = false;
-    console.log("First for loop notPrime::: " + notPrime);
     for (var i = 2; i <= counter; i++) {
-      console.log("i::: " + i);
-      console.log("counter::: " + counter);
-      console.log(counter + " % " + i + "=== 0 = " + (counter % i === 0));
-      console.log(i + "!===" + counter + "=" + (i !== counter));
       if (counter % i === 0 && i !== counter) {
         notPrime = true;
       }
-      console.log("Inside second for loop notPrime::: " + notPrime);
     }
-    console.log("Outside second for loop notPrime::: " + notPrime);
     if (notPrime === false) {
       console.log("Prime Number: " + counter);
     }
