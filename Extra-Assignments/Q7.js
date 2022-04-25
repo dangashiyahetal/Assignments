63dbcccf1e551baea40df2ffa423c7bc1f8d2838
Hetal Dangashiya <dangashiyahetal@gmail.com>
2022-04-25 12:07:14 -0400

    Minor Changes

--- Extra-Assignments/Q7.js
+++ Extra-Assignments/Q7.js
@@ -1,6 +1,4 @@
 function infotransfer() {
-  var para=Aim for three to five or more sentences per paragraph.<br>
-  Include on each page about two handwritten or three typed<br>
-  paragraphs.
+  var para="Aim for three to five or more sentences per paragraph.<br>Include on each page about two handwritten or three typed<br>paragraphs.";
 }
 
