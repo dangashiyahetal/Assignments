179107cef480e188d0137ae070e76284bcb59b72
Hetal Dangashiya <dangashiyahetal@gmail.com>
2022-04-27 16:17:38 -0400

    changes

--- Extra-Assignments/Q7.js
+++ Extra-Assignments/Q7.js
@@ -1,6 +1,19 @@
 function infotransfer() {
   var para =
     "Aim for three to five or more sentences per paragraph.<br>Include on each page about two handwritten or three typed<br>paragraphs.";
+    var div1Element = document.getElementById("div1");
   var element = document.createElement("img");
+  element.setAttribute("src","image2.jpg");
+  div1Element.appendChild(element);
+  var elePara = document.createElement("p");
+  elePara.innerHTML = para;
+  div1Element.appendChild(elePara);
 }
-function diisplayDiv2() {}
+function diisplayDiv2() {
+  var para =
+    "Paragraph small div";
+    var div1Element = document.getElementById("div2");
+    var elePara = document.createElement("div");
+  elePara.innerHTML = para;
+  div1Element.appendChild(elePara);
+}
