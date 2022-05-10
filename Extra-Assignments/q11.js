bb8bc005483e5f1778314ae1636e535424b4b302
Hetal Dangashia <dangashiyahetal@gmail.com>
2022-05-10 15:00:17 -0400

    Q11

--- Extra-Assignments/q11.js
+++ Extra-Assignments/q11.js
@@ -1,12 +1,32 @@
 function firstfield() {
-    var x = document.getElementById("text1");
-    if (x.value.length >= 1) {
-        if (!isNaN(x.value)) {
-            alert("number");
-        } else {
-            alert("string");
-        }
+  var x = document.getElementById("text1");
+  if (x.value.length >= 1) {
+    if (!isNaN(x.value)) {
+      alert("number");
     } else {
-        alert("null");
+      alert("string");
     }
+  } else {
+    alert("null");
+  }
+}
+function displaySelectedcheckbx() {
+  var box = document.getElementsByName("option");
+  for (let index = 0; index < box.length; index++) {
+    if (box[index].checked) {
+      alert("selected");
+      break;
+    }
+  }
+}
+function displaycheckbx() {
+  var box = document.getElementById("checkbx");
+  if (box.checked) {
+    document.getElementById("divCheckbox").innerHTML = "Checked";
+  } else {
+    document.getElementById("divCheckbox").innerHTML = "Unchecked";
+  }
+}
+function submitForm() {
+  alert("the form has been successfully submitted");
 }
