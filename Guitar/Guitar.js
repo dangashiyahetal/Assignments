bf0388b48192233f39031bc3ebbb44d8f4aa2fea
Hetal Dangashia <dangashiyahetal@gmail.com>
2022-05-25 12:53:01 -0400

    changes

--- Guitar/Guitar.js
+++ Guitar/Guitar.js
@@ -1,6 +1,10 @@
-
-var count=0
-function previous(){
-count=count ==3 ? 0=count;
-
-}
+var guitardata = {};
+const http = new XMLHttpRequest();
+http.open("GET", "./assets/guitardata.json");
+http.send();
+http.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    guitardata = http.responseText;
+    console.log(JSON.parse(guitardata).allProducts[0]);
+  }
+};
