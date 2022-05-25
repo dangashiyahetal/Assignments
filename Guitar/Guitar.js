8a46ce7074c3ae229d6af4e621390bc993b16c9f
Hetal Dangashia <dangashiyahetal@gmail.com>
2022-05-25 14:47:27 -0400

    changes

--- Guitar/Guitar.js
+++ Guitar/Guitar.js
@@ -1,10 +1,22 @@
 var guitardata = {};
+var count = 0;
 const http = new XMLHttpRequest();
 http.open("GET", "./assets/guitardata.json");
 http.send();
 http.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
     guitardata = http.responseText;
-    console.log(JSON.parse(guitardata).allProducts[0]);
+    console.log(JSON.parse(guitardata).allProducts);
+    next(JSON.parse(guitardata).allProducts);
   }
 };
+
+function next(data) {
+  document.getElementById("displayImg").src = data[count].image_path;
+  document.getElementById("product-description").innerHTML =
+    data[count].product_description;
+  document.getElementById("shipping-info").innerHTML =
+    data[count].shipping_details;
+  document.getElementById("customer_review").innerHTML =
+    data[count].customer_reviews;
+}
